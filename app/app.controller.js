cb768504021c960fc6c6fbd07c806db81dcd25b0
Cam Couch <cameronmcouch@gmail.com>
2019-02-08 18:12:47 -0500

    these are the Angular lab updates for part 2 - PLEASE LOOK AT 2/7 commit for initial grading

--- app/app.controller.js
+++ app/app.controller.js
@@ -8,6 +8,15 @@
             {task: "Hang out with Kristen", completed: true},
             {task: "Pet the cats", completed: true}
         ];
+        vm.addTask = function(newTask) {
+            vm.tasks.push(angular.copy(newTask));
+            vm.newTodo = {};
     }
-
+        vm.removeTask = function(index) {
+            vm.tasks.splice(index, 1);
+        }
+        vm.completeTask = function(todo) {
+            todo.completed = !todo.completed;
+        }
+}
 angular.module("todoApp").controller("TodoController", TodoController);
